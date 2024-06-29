218e53f63b5286046c52117b86701872da99acd1
Sophie <sallysophie@hotmail.de>
2024-06-29 11:57:14 +0200

    final version with score & life count

--- js/game.js
+++ js/game.js
@@ -1,3 +1,88 @@
 class Game {
-    // code to be added
+  // code to be added
+  constructor() {
+    this.startScreen = document.querySelector("#game-intro");
+    this.gameScreen = document.querySelector("#game-screen");
+    this.gameEndScreen = document.querySelector("#game-end");
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+    this.gameIntervalId = null;
+    this.gameLoopFrequency = Math.round(1000 / 60);
+    this.player = new Player(
+      this.gameScreen,
+      200,
+      500,
+      100,
+      150,
+      "../images/car.png"
+    );
+    this.playersScore = document.querySelector("#score");
+    this.playersLifes = document.querySelector("#lives");
+  }
+  start() {
+    this.gameScreen.style.width = this.width + "px";
+    this.gameScreen.style.height = this.height + "px";
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrequency);
+  }
+
+  gameLoop() {
+    this.update();
+    this.gameIsOver && clearInterval(this.gameIntervalId); // if statement
+  }
+
+  update() {
+    this.player.move();
+
+    // Check for collision and if an obstacle is still on the screen
+    for (let i = 0; i < this.obstacles.length; i++) {
+      const obstacle = this.obstacles[i];
+      obstacle.move();
+
+      // If the player's car collides with an obstacle
+      if (this.player.didCollide(obstacle)) {
+        obstacle.element.remove();
+        this.obstacles.splice(i, 1);
+        this.lives--;
+        this.playersLifes.innerText = this.lives;
+        i--;
+      }
+      // If the obstacle is off the screen (at the bottom)
+      else if (obstacle.top > this.height) {
+        this.score++;
+        this.playersScore.innerText = this.score;
+        obstacle.element.remove();
+        this.obstacles.splice(i, 1);
+        i--;
+      }
+    }
+
+    // If the lives are 0, end the game
+    if (this.lives === 0) {
+      this.endGame();
+    }
+
+    // Create a new obstacle based on a random probability
+    // when there is no other obstacles on the screen
+    if (Math.random() > 0.98 && this.obstacles.length < 1) {
+      this.obstacles.push(new Obstacle(this.gameScreen));
+    }
+  }
+
+  endGame() {
+    this.player.element.remove();
+    this.obstacles.forEach((object) => {
+      object.element.remove();
+    });
+    this.gameIsOver = true;
+    this.gameScreen.style.display = "none";
+    this.gameEndScreen.style.display = "block";
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,58 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game = null;
+
+  // Function that handles keydown event
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    // Check if the pressed key is in the possibleKeystrokes array
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      // Update player's directionX and directionY based on the key pressed
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+    }
+  }
 
   startButton.addEventListener("click", function () {
     startGame();
   });
 
   function startGame() {
+    game = new Game();
+    game.start();
     console.log("start game");
+  }
+
+  // Add the handleKeydown function as an event listener for the keydown event
+  window.addEventListener("keydown", handleKeydown);
+
+  restartButton.addEventListener("click", () => {
+    restartGame();
+  });
+
+  function restartGame() {
+    location.reload();
   }
 };
